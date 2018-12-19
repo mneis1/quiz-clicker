f9139b304708a23ca47e42045a5799116f37322c
tazer101 <deetunemylife@gmail.com>
2018-12-19 11:19:31 -0500

    trying to clean ui

--- client/src/components/View.js
+++ client/src/components/View.js
@@ -4,8 +4,6 @@ import Provider from "react-redux/es/components/Provider";
 import StudentView from "./StudentView";
 import AppNavBar from "./AppNavbar";
 import TeacherView from "./TeacherView";
-import {verifyStudent, verifyTeacher} from "../actions/userActions";
-import {connect} from 'react-redux';
 
 class View extends Component {
     state = {
@@ -13,32 +11,20 @@ class View extends Component {
         teacher: false
     };
 
-    componentDidMount() {
-        this.props.verifyStudent(this.props.token);
-        this.props.verifyTeacher(this.props.token);
-        if (!this.props.token) {
-            this.setState({redirect: true});
-        }
-        this.props.getCourses(this.props.token);
-    }
-
     render () {
         return (
             <Provider store={store}>
                 <div className="View">
-
+                    {
+                        this.state.teacher ?  <TeacherView/> : <div/>
+                    }
                     <AppNavBar/>
                     <StudentView/>
-                    {this.state.teacher ? <TeacherView/> : <div/>}
+
                 </div>
             </Provider>
         );
     }
 }
 
-const mapStateToProps = (state) =>({
-    token: state.users.token,
-    teacher: state.users.teacher
-});
-
-export default connect(mapStateToProps)(View);
+export default View;
